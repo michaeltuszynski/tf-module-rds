75588913bd8b93cfb52701243344b1e1de190b72
Michael Tuszynski <mpt@The-Sanctum.local>
2024-08-01 17:03:29 -0700

    first commit

---
+++ main.tf
@@ -0,0 +1,116 @@
+## Secrets for DB
+resource "random_integer" "password_length" {
+  min = 8
+  max = 16
+}
+
+resource "random_password" "db_password" {
+  length  = random_integer.password_length.result
+  special = false
+}
+
+resource "random_string" "suffix" {
+  length  = 5
+  special = false
+  upper   = false
+  numeric = true
+  lower   = true
+}
+
+resource "aws_secretsmanager_secret" "db_credentials" {
+  name        = "${var.project_name}-db-credentials-${random_string.suffix.result}"
+  description = "DB credentials for the ${var.project_name} project"
+  tags        = var.tags
+}
+
+resource "aws_secretsmanager_secret_version" "db_secret_version" {
+  secret_id = aws_secretsmanager_secret.db_credentials.id
+  secret_string = jsonencode({
+    username = "root"
+    password = random_password.db_password.result
+  })
+}
+
+resource "aws_security_group" "rds_mysql_sg" {
+  vpc_id      = var.vpc_id
+  name        = "${var.project_name}-rds-sg"
+  description = "Security group for the ${var.project_name} RDS"
+
+  ingress {
+    from_port   = var.mysql_db_port
+    to_port     = var.mysql_db_port
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr_block]
+    description = "${var.project_name}-db-port"
+  }
+
+  tags = var.tags
+}
+
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  name       = "rds-subnet-group"
+  subnet_ids = var.private_subnet_ids
+
+  tags = merge(
+    {
+      Name = "rds-subnet-group"
+    },
+    var.tags
+  )
+}
+
+# create iam role for enhanced monitoring
+resource "aws_iam_role" "rds_monitoring_role" {
+  name = "rds-monitoring-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "monitoring.rds.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# create iam policy attachment for enhanced monitoring
+resource "aws_iam_role_policy_attachment" "rds_monitoring_policy_attachment" {
+  role       = aws_iam_role.rds_monitoring_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
+}
+
+resource "aws_db_instance" "mysql_db" {
+  identifier                  = var.db_identifier
+  allocated_storage           = var.allocated_storage
+  max_allocated_storage       = var.max_allocated_storage
+  monitoring_interval         = 60
+  monitoring_role_arn         = aws_iam_role.rds_monitoring_role.arn
+  storage_type                = "gp3"
+  engine                      = "mysql"
+  engine_version              = "8.0.35"
+  instance_class              = "db.t3.micro"
+  db_name                     = var.db_identifier
+  parameter_group_name        = "default.mysql8.0"
+  skip_final_snapshot         = true
+  vpc_security_group_ids      = ["${aws_security_group.rds_mysql_sg.id}"]
+  db_subnet_group_name        = aws_db_subnet_group.rds_subnet_group.name
+  publicly_accessible         = true
+  ca_cert_identifier          = "rds-ca-rsa2048-g1"
+  allow_major_version_upgrade = true
+  auto_minor_version_upgrade  = true
+
+  # Pointing to the secrets manager for credentials
+  username = jsondecode(aws_secretsmanager_secret_version.db_secret_version.secret_string)["username"]
+  password = jsondecode(aws_secretsmanager_secret_version.db_secret_version.secret_string)["password"]
+
+  # Backup settings
+  backup_retention_period = 7                     # Keep backups for 7 days
+  backup_window           = "07:00-09:00"         # Preferred backup window
+  maintenance_window      = "sun:05:00-sun:06:00" # Preferred maintenance window
+
+  tags = var.tags
+}
---
+++ variables.tf
@@ -0,0 +1,55 @@
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+  default     = "app-vpc"
+}
+
+variable "tags" {
+  description = "A map of tags to apply to the resources."
+  type        = map(string)
+  default     = {}
+}
+
+variable "vpc_id" {
+  description = "The VPC ID"
+  type        = string
+}
+
+variable "vpc_cidr_block" {
+  description = "The CIDR block for the VPC"
+  type        = string
+}
+
+variable "mysql_db_port" {
+  description = "The port the MySQL database will listen on"
+  type        = number
+  default     = 3306
+}
+
+variable "private_subnet_ids" {
+  description = "The IDs of the private subnets"
+  type        = list(string)
+}
+
+variable "db_identifier" {
+  description = "The identifier for the RDS instance"
+  type        = string
+
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9]+$", var.db_identifier))
+    error_message = "The db_identifier must be alphanumeric only."
+  }
+}
+
+variable "allocated_storage" {
+  description = "The amount of storage to allocate to the RDS instance"
+  type        = number
+  default     = 20
+}
+
+variable "max_allocated_storage" {
+  description = "The maximum amount of storage to allocate to the RDS instance"
+  type        = number
+  default     = 100
+}
+
